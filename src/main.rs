a93b53973f85844789cdb724bb1f71ebf6b08066
Ethan Waxman <ejw99@cornell.edu>
2026-02-22 18:21:16 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,11 @@ use std::io::{self, Write};
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+    
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).expect("Issue reading from stdin");
+    
+    let line = input.trim();
+    println!("{}: command not found", line);
 }
