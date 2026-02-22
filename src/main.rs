8cf6fbfab6ec47726bf2a7ab257b499f9a5ea654
Ethan Waxman <ejw99@cornell.edu>
2026-02-22 18:55:22 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+
 fn main() {
     loop {
         print!("$ ");
@@ -12,10 +13,20 @@ fn main() {
         
         let line = input.trim();
 
+        // Hardcode exit to get out of loop
         if line.starts_with("exit") {
             break;
         }
 
+        eval(line);
+    }
+}
+
+fn eval(line: &str) {
+    if line.starts_with("echo ") {
+        let text = &line[5..];
+        println!("{}", text);
+    } else {
         println!("{}: command not found", line);
     }
 }
