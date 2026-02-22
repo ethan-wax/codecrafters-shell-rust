ea5f0bdbaa9966f5508cfefc5714f18b0203e157
Ethan Waxman <ejw99@cornell.edu>
2026-02-22 18:25:00 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,11 @@ fn main() {
         io::stdin().read_line(&mut input).expect("Issue reading from stdin");
         
         let line = input.trim();
+
+        if line.starts_with("exit") {
+            break;
+        }
+
         println!("{}: command not found", line);
     }
 }
